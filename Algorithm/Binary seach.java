41ffa7be5096b420ccff6bf5d6ea69b3d0d02073
som3a <depalh19@gmail.com>
2024-10-12 00:05:59 +0300

    sort array

--- Algorithm/Binary seach.java
+++ Algorithm/Binary seach.java
@@ -22,7 +22,7 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        int []arr={1,5,8,97,4};
+        int []arr={1,4,5,8,97}; // binary search work in sorted array or sorted range
         int person=97;
         int pos=binarySearch(arr,person);// will return the index if the element is found or -1 if not
         System.out.println("Is founded on  : "+pos); // 3
